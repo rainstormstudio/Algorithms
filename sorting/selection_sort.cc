a5bff8e9e3bc5775a3142fda8055257e0225bd6c
Daniel <rainstormstudio@yahoo.com>
2020-10-17 18:29:06 -0400

    selection sort

---
+++ sorting/selection_sort.cc
@@ -0,0 +1,54 @@
+/**
+ * @file selection_sort.cc
+ * @author Hongyu Ding
+ * @brief This is the code of the selection sort
+ * @version 1.0
+ * @date 2020-10-17
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <iostream>
+#include <vector>
+
+/**
+ * @brief selection sorts the array a
+ * O(n^2)
+ * 
+ * @param a the array we want to perform selection sort with
+ */
+void selection_sort(std::vector<int> &a) {
+    int len = a.size();
+    for (int i = 0; i < len - 1; i ++) {
+        for (int j = i + 1; j < len; j ++) {
+            if (a[i] > a[j]) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    std::vector<int> array;
+    int n;
+    
+    // input
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int val;
+        std::cin >> val;
+        array.push_back(val);
+    }
+
+    selection_sort(array);
+
+    // output
+    for (int i = 0; i < n; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
