f5f1ba011e052f875b5186e41dc2a4495d395a5a
Daniel <rainstormstudio@yahoo.com>
2020-12-01 20:15:36 -0500

    linear system solver C++ use matrix class

--- math/C++/linear_system_solver.cc
+++ math/C++/linear_system_solver.cc
@@ -14,7 +14,234 @@
 #include <iomanip>
 
 using Vector = std::vector<double>;
-using Matrix = std::vector<Vector>;
+
+
+class Matrix {
+    std::vector<std::vector<double>> mat;
+    unsigned int rows;
+    unsigned int cols;
+public:
+    Matrix(std::vector<std::vector<int>> ary) {
+        rows = ary.size();
+        cols = ary[0].size();
+        mat = std::vector<std::vector<double>>(rows);
+        for (int i = 0; i < rows; i ++) {
+            mat[i] = std::vector<double>(cols);
+            for (int j = 0; j < cols; j ++) {
+                mat[i][j] = ary[i][j];
+            }
+        }
+    }
+
+    Matrix(unsigned int rows, unsigned int cols) 
+        : rows{rows}, cols{cols} {
+        mat = std::vector<std::vector<double>>(rows);
+        for (int i = 0; i < rows; i ++) {
+            mat[i] = std::vector<double>(cols);
+            for (int j = 0; j < cols; j ++) {
+                mat[i][j] = 0.0;
+            }
+        }
+    }
+
+    Matrix(unsigned int rows, unsigned int cols, double val) 
+        : rows{rows}, cols{cols} {
+        mat = std::vector<std::vector<double>>(rows);
+        for (int i = 0; i < rows; i ++) {
+            mat[i] = std::vector<double>(cols);
+            for (int j = 0; j < cols; j ++) {
+                mat[i][j] = val;
+            }
+        }
+    }
+
+    Matrix(std::istream &in) {
+        in >> rows;
+        in >> cols;
+        mat = std::vector<std::vector<double>>(rows);
+        for (int i = 0; i < rows; i ++) {
+            mat[i] = std::vector<double>(cols);
+            for (int j = 0; j < cols; j ++) {
+                in >> mat[i][j];
+            }
+        }
+    }
+
+    Matrix(const Matrix &mat2) {
+        rows = mat2.getRows();
+        cols = mat2.getCols();
+        mat = std::vector<std::vector<double>>(rows);
+        for (int i = 0; i < rows; i ++) {
+            mat[i] = std::vector<double>(cols);
+            for (int j = 0; j < cols; j ++) {
+                mat[i][j] = mat2[i][j];
+            }
+        }
+    }
+
+    int getRows() const { return rows; }
+
+    int size() const { return rows; }
+
+    int getCols() const { return cols; }
+
+    std::vector<std::vector<double>> getMat() {
+        return mat;
+    }
+
+    std::vector<double>& operator[](std::size_t index) {
+        return mat[index];
+    }
+
+    const std::vector<double>& operator[](std::size_t index) const {
+        return mat[index];
+    }
+
+    Matrix& operator=(const Matrix& mat2) {
+        if (this != &mat2) {
+            rows = mat2.getRows();
+            cols = mat2.getCols();
+            mat.resize(rows);
+            for (int i = 0; i < rows; i ++) {
+                mat[i].resize(cols);
+            }
+            for (int i = 0; i < rows; i ++) {
+                for (int j = 0; j < cols; j ++) {
+                    mat[i][j] = mat2[i][j];
+                }
+            }
+        }
+        return *this;
+    }
+
+    Matrix operator+(const Matrix& mat2) {
+        Matrix ans(rows, cols);
+        if (rows == mat2.getRows() && cols == mat2.getCols()) {
+            for (int i = 0; i < rows; i ++) {
+                for (int j = 0; j < cols; j ++) {
+                    ans[i][j] = mat[i][j] + mat2[i][j];
+                }
+            }
+        }
+        return ans;
+    }
+
+    Matrix& operator+=(const Matrix& mat2) {
+        if (rows == mat2.getRows() && cols == mat2.getCols()) {
+            for (int i = 0; i < rows; i ++) {
+                for (int j = 0; j < cols; j ++) {
+                    mat[i][j] += mat2[i][j];
+                }
+            }
+        }
+        return *this;
+    }
+
+    Matrix operator*(const Matrix& mat2) {
+        Matrix ans(rows, mat2.getCols());
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < mat2.getCols(); j ++) {
+                for (int k = 0; k < cols; k ++) {
+                    ans[i][j] += mat[i][k] * mat2[k][j];
+                }
+            }
+        }
+        return ans;
+    }
+
+    Matrix& operator*=(const Matrix& mat2) {
+        Matrix ans = (*this) * mat2;
+        (*this) = ans;
+        return *this;
+    }
+
+    Matrix operator+(const double val) {
+        Matrix ans(rows, cols);
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < cols; j ++) {
+                ans[i][j] = mat[i][j] + val;
+            }
+        }
+        return ans;
+    }
+
+    Matrix operator-(const double val) {
+        Matrix ans(rows, cols);
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < cols; j ++) {
+                ans[i][j] = mat[i][j] - val;
+            }
+        }
+        return ans;
+    }
+
+    Matrix operator*(const double val) {
+        Matrix ans(rows, cols);
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < cols; j ++) {
+                ans[i][j] = mat[i][j] * val;
+            }
+        }
+        return ans;
+    }
+
+    Matrix operator/(const double val) {
+        Matrix ans(rows, cols);
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < cols; j ++) {
+                ans[i][j] = mat[i][j] / val;
+            }
+        }
+        return ans;
+    }
+
+    std::vector<int> operator*(const std::vector<double> &vec) {
+        std::vector<int> ans(vec.size(), 0.0);
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < cols; j ++) {
+                ans[i] = mat[i][j] * vec[j];
+            }
+        }
+        return ans;
+    }
+
+    Matrix transpose() {
+        Matrix ans(cols, rows);
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < cols; j ++) {
+                ans[j][i] = mat[i][j];
+            }
+        }
+        return ans;
+    }
+
+    static Matrix identity(int n) {
+        Matrix ans(n, n);
+        for (int i = 0; i < n; i ++) {
+            ans[i][i] = 1;
+        }
+        return ans;
+    }
+
+    std::vector<double> getDiagonal() {
+        int n = std::min(rows, cols);
+        std::vector<double> ans(n, 0.0);
+        for (int i = 0; i < n; i ++) {
+            ans[i] = mat[i][i];
+        }
+        return ans;
+    }
+
+    void output(std::ostream &out, int width, int precision) {
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < cols; j ++) {
+                out << std::setw(width) << std::setprecision(precision) << mat[i][j];
+            }
+            out << std::endl;
+        }
+    }
+};
+
 
 class LSsolver {
 
@@ -32,28 +259,6 @@ public:
         for (int i = 0; i < len; i ++) {
             std::cout << std::setw(10);
             std::cout << vec[i] << std::endl;
-        }
-    }
-
-    static Matrix inputMatrix(int n, int m) {
-        Matrix mat = Matrix(n);
-        for (int i = 0; i < n; i ++) {
-            mat[i] = Vector(m, 0);
-            for (int j = 0; j < m; j ++) {
-                std::cin >> mat[i][j];
-            }
-        }
-        return mat;
-    }
-
-    static void outputMatrix(Matrix &mat) {
-        int n = mat.size(), m = mat[0].size();
-        for (int i = 0; i < n; i ++) {
-            for (int j = 0; j < m; j ++) {
-                std::cout << std::setw(10);
-                std::cout << mat[i][j];
-            }
-            std::cout << std::endl;
         }
     }
 
@@ -135,24 +340,16 @@ public:
 };
 
 int main() {
-    Matrix A;
-    Vector b;
-    int n, m;
     std::cout << "solve linear system Ax = b:\n";
     std::cout << "input A:\n";
-    std::cout << "n = ";
-    std::cin >> n;
-    std::cout << "m = ";
-    std::cin >> m;
-    std::cout << "matrix = \n";
-    A = LSsolver::inputMatrix(n, m);
+    Matrix A(std::cin);
     std::cout << "input b:\n";
-    b = LSsolver::inputVector(n);
+    Vector b = LSsolver::inputVector(A.size());
 
     Vector x = LSsolver::solve(A, b);
 
     std::cout << "L & U:\n";
-    LSsolver::outputMatrix(A);
+    A.output(std::cout, 10, 5);
     std::cout << "new b:\n";
     LSsolver::outputVector(b);
     std::cout << "ans x = \n";
